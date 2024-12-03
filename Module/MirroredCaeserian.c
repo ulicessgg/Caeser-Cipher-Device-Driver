0bb1e0df932754d4929bdb0c759b6777e403a679
Ulices Gonzalez <sgonzalez30@sfsu.edu>
2024-12-02 16:01:22 -0800

    edited makefile in test and module and created c files before translating psuedocode, first will refer to videos to set up kernel

---
+++ Module/MirroredCaeserian.c
@@ -0,0 +1,12 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2024
+* Name:: Ulices Gonzalez
+* Student ID:: 923328897
+* GitHub-Name:: ulicessgg
+* Project:: Assignment 6 – Device Driver
+*
+* File:: <MirroredCaeserian.c>
+*
+* Description:: 
+*
+**************************************************************/
---
+++ Test/Gonzalez_Ulicesz_HW6_main.c
@@ -0,0 +1,22 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2024
+* Name:: Ulices Gonzalez
+* Student ID:: 923328897
+* GitHub-Name:: ulicessgg
+* Project:: Assignment 6 – Device Driver
+*
+* File:: <Gonzalez_Ulices_HW6_main.c>
+*
+* Description:: 
+*
+**************************************************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
